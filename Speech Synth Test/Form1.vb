f8a04621dedfcc3eb99c7ce0d95400e1ed0e1efb
Luke W <law39@aber.ac.uk>
2018-06-13 00:13:54 +0100

    Initial Commit
    Initial commit of program to main branch

---
+++ Speech Synth Test/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub GoBTN_Click(sender As Object, e As EventArgs) Handles GoBTN.Click
+
+        Dim name As String                                      'Initialises variables
+        Dim sentence As String
+        Dim StrPhrase As String
+        Dim ObjSpeech As Object
+        Dim intPitch As Integer
+
+        name = (InputBox("Please enter your name"))             'Inputs name
+
+        If (name) = ("") Then                                   'If name is blank say don't know else state name
+            sentence = ("I don't know your name")
+        Else
+            sentence = ("Your name is " & name)
+        End If
+
+
+        MsgBox(sentence)                                        'State sentence to be spoken
+
+        ObjSpeech = (CreateObject("SAPI.SpVoice"))              'Speech Code
+        StrPhrase = (sentence)
+        ObjSpeech.Speak("<pitch middle = '" & intPitch & "'/>" + StrPhrase)
+
+    End Sub
+End Class
